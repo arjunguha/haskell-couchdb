54f25f407596879eeb21bc09879ac25d9575ee6d
Arjun Guha <arjun+browncs@cs.brown.edu>
2008-09-03 13:21:40 -0400

    docs on peabody

--- doc.sh
+++ doc.sh
@@ -3,7 +3,7 @@
 if [ `hostname` = "wanderlust.local" ]; then
   open /Users/arjun/local/share/doc/CouchDB-0.8.0/html/index.html
 elif [ `hostname` = "peabody" ]; then
-  echo "Building on peabody ..."
+ firefox /pro/plt/ghc/latest/share/doc/CouchDB-0.8.0.1/html/index.html &
 else
   echo "ERROR: Don't know how to build on " `hostname`
 fi
