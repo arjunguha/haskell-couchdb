410168a98c1201ba6260d32f8c9c641559e83cb0
Arjun Guha <arjun+browncs@cs.brown.edu>
2008-09-10 14:05:54 -0400

    JSON instances for sanitized Doc and DB names

--- src/Database/CouchDB/Safety.hs
+++ src/Database/CouchDB/Safety.hs
@@ -11,12 +11,24 @@ module Database.CouchDB.Safety
   ) where
 
 import Data.List (elem)
+import Database.CouchDB.JSON
+import Text.JSON
 
 -- |Database name
 data DB = DB String
 
 instance Show DB where
   show (DB s) = s
+
+instance JSON DB where
+  readJSON val = do
+    s <- readJSON val
+    case isDBString s of
+      False -> fail "readJSON: not a valid database name"
+      True -> return (DB s)
+
+  showJSON (DB s) = showJSON s
+
 
 isDBChar ch = (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') 
     || (ch >= '0' && ch <= '9')
@@ -43,6 +55,15 @@ data Doc = Doc String
 instance Show Doc where
   show (Doc s) = s
 
+instance JSON Doc where
+  readJSON val = do
+    s <- readJSON val
+    case isDocString s of
+      False -> fail "readJSON: not a valid document name"
+      True -> return (Doc s)
+
+  showJSON (Doc s) = showJSON s
+
 -- |Returns a safe document name.  Signals an error if the name is
 -- invalid.
 doc :: String -> Doc
@@ -53,3 +74,5 @@ doc docName = case isDocString docName of
 isDocString :: String -> Bool
 isDocString [] = False
 isDocString (first:rest) = isFirstDocChar first && and (map isDocChar rest)
+
+
