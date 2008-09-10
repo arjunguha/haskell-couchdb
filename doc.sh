7dd7ad8a53f5d7de0a919404e007b590486bb49a
Arjun Guha <arjun+browncs@cs.brown.edu>
2008-09-10 15:38:25 -0400

    updated build and doc

--- doc.sh
+++ doc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ `hostname` = "wanderlust.local" ]; then
-  open /Users/arjun/local/share/doc/CouchDB-0.8.0/html/index.html
+  open /Users/arjun/local/share/doc/CouchDB-0.8.0.4/html/index.html
 elif [ `hostname` = "peabody" ]; then
- firefox /pro/plt/ghc/latest/share/doc/CouchDB-0.8.0.1/html/index.html &
+ firefox /pro/plt/ghc/latest/share/doc/CouchDB-0.8.0.4/html/index.html &
 else
   echo "ERROR: Don't know how to build on " `hostname`
 fi
