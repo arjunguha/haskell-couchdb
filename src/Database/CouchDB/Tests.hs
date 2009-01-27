1fcc4a9ad4fce8d88ad63175a7ec8c2a92c26a0e
Arjun Guha <arjun@cs.brown.edu>
2009-01-27 10:04:26 -0500

    slightly better testing

--- src/Database/CouchDB/Tests.hs
+++ src/Database/CouchDB/Tests.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -XFlexibleInstances #-}
 module Database.CouchDB.Tests ( main, allTests) where
 
+import Control.Monad.Trans (liftIO)
+import Control.Exception (finally)
 import Test.HUnit
 import Database.CouchDB
 import Database.CouchDB.JSON
@@ -32,6 +34,18 @@ assertJust (Just v) = return v
 assertJust Nothing = do
   assertFailure "expected (Just ...), got Nothing"
   fail "assertion failed"
+
+testWithDB :: String -> (DB -> CouchMonad Bool) -> Test
+testWithDB testDescription testCase = 
+  TestLabel testDescription $ TestCase $ do
+    let action = runCouchDB' $ do
+          createDB "haskellcouchdbtest"
+          result <- testCase (db "haskellcouchdbtest")
+          liftIO $ assertBool testDescription result
+    let teardown = runCouchDB' (dropDB "haskellcouchdbtest") 
+    let failure _ = assertFailure (testDescription ++ "; exception signalled")
+    action `catch` failure `finally` teardown
+    
 
 main = do
   putStrLn "Running CouchDB test suite..."
@@ -72,17 +86,13 @@ testCreate = TestCase $ assertDBEqual "create/drop database" True $ do
 
 people = [ Age "Arjun" 18, Age "Alex" 17 ]
 
-testNamedDocs = TestCase $ assertDBEqual "add named documents" people $ do
-  createDB "test1"
-  let mydb = db "test1"
+testNamedDocs = testWithDB "add named documents" $ \mydb -> do
   newNamedDoc mydb (doc "arjun") (people !! 0)
   newNamedDoc mydb (doc "alex") (people !! 1)
   Just (_,_,v1) <- getDoc mydb (doc "arjun")
   Just (_,_,v2) <- getDoc mydb (doc "alex") 
-  dropDB "test1"
-  return [v1, v2]
+  return $ (v1 == people !! 0) && (v2 == people !! 1)
+
   
-
-
 allTests = TestList [ testCreate, testNamedDocs ]
 
