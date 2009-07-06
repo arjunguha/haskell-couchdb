3f0119647b0f9404eaadb0bdd9215703df5fc6fe
Martin Hilbig <blueonyx@void-system.localdomain>
2009-07-06 23:58:04 +0200

    added "/_design/" to view url

--- src/Database/CouchDB/Unsafe.hs
+++ src/Database/CouchDB/Unsafe.hs
@@ -286,7 +286,7 @@ queryView :: (JSON a)
           -> CouchMonad [(JSString, a)]
 queryView db viewSet view args = do
   let args' = map (\(k,v) -> (k,encode v)) args
-  let url' = concat [db, viewSet, "/_view/", view]
+  let url' = concat [db, "/_design/", viewSet, "/_view/", view]
   r <- request url' args' GET [] ""
   case rspCode r of
     (2,0,0) -> do
@@ -304,7 +304,7 @@ queryViewKeys :: String  -- ^database
             -> CouchMonad [String]
 queryViewKeys db viewSet view args = do
   let args' = map (\(k,v) -> (k,encode v)) args
-  let url' = concat [db, viewSet, "/_view/", view]
+  let url' = concat [db, "/_design/", viewSet, "/_view/", view]
   r <- request url' args' GET [] ""
   case rspCode r of
     (2,0,0) -> do
