302f7c57f031dd822b08d910f598390426605346
Arjun Guha <arjun.guha@gmail.com>
2012-03-07 12:40:49 -0500

    removed machine-dependent scripts

--- doc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ `hostname` = "wanderlust.local" ]; then
-  open /Users/arjun/local/share/doc/CouchDB-0.8.0.4/html/index.html
-elif [ `hostname` = "peabody" ]; then
- firefox /pro/plt/ghc/latest/share/doc/CouchDB-0.8.0.4/html/index.html &
-else
-  echo "ERROR: Don't know how to build on " `hostname`
-fi
